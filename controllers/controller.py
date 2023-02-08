c573216eaf299d275c686c57a13fe7b9b3486fb1
Shazreen Seatter <shazseatter12@gmail.com>
2023-02-08 14:53:05 +0000

    made order list work in browser to display customer book orders

---
+++ controllers/controller.py
@@ -0,0 +1,12 @@
+from flask import render_template
+from app import app
+from models.order_list import orders
+
+
+@app.route('/')
+def index():
+    return "Hello World"
+
+@app.route("/orders")
+def order_index():
+    return render_template('index.html', orders=orders)
---
+++ models/order_list.py
@@ -0,0 +1,6 @@
+from models.order import *
+
+order1 = Order("John Smith", "8 February 2023", "Lord of the Rings", 1)
+order2 = Order("Helen Smith", "8 February 2023", "The Hobbit", 1)
+
+orders = [order1, order2]
